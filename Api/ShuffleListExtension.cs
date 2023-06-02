e0c28825374a9efd721481e245254c815ed479cd
Glen Souza <glenster75@gmail.com>
2023-06-01 23:01:17 -0700

    Create ShuffleListExtension.cs

---
+++ Api/ShuffleListExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Api;
+
+public static class ShuffleListExtension
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        if (list == null) throw new ArgumentNullException(nameof(list));
+        int n = list.Count;
+        while (n > 1)
+        {
+            int k = Random.Shared.Next(n--);
+            (list[n], list[k]) = (list[k], list[n]);
+        }
+    }
+}
